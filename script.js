1fe20415c7ea58ab8f277ccbfa069f3b9ec0d34e
Michael <80859948+bridgemami@users.noreply.github.com>
2021-10-19 16:48:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.body.style.backgroundColor="yellow";
+
+document.getElementById('1').innerHTML="<h1>Welcome to this basic JS Page</h1><p>";
+document.getElementById('1').style.color="green";
+document.getElementById('1').style.fontFamily="sans-serif";
+
